89d150f46f4daf9a69f4a06a2354dffe30d7a2f0
omkar <omkar.jejurkar@bito.ai>
2024-03-12 20:48:03 +0530

    added reactions over messages and changed some stylings

--- src/components/MessageModel.jsx
+++ src/components/MessageModel.jsx
@@ -3,6 +3,7 @@ import styles from "./MessageModel.module.css";
 import { GiCrossMark } from "react-icons/gi";
 import { RiUserFill } from "react-icons/ri";
 import MsgButton from "./MsgButton";
+import { AiOutlineSend } from "react-icons/ai";
 
 function MessageModel(props) {
   console.log("message model painted");
@@ -33,6 +34,30 @@ function MessageModel(props) {
     console.log(props.msgArrived);
     e.preventDefault();
     props.sendMessage(e.target.message.value);
+    e.target.message.value = "";
+  }
+
+  function deleteMessage(id) {
+    let url = "http://localhost:8081/deleteMessageById/" + id;
+    console.log("!!!!!!!!!!!!!!!!!!!!!!!!" + id);
+    fetch(url, {
+      method: "DELETE",
+      mode: "cors",
+      body: "",
+      headers: { "Content-Type": "application/json" },
+    }).then((response) => getMessagesFromServer());
+  }
+
+  function setReaction(messageId, reactionId) {
+    let url =
+      "http://localhost:8081/setReaction/" + messageId + "/" + reactionId;
+    console.log("---------------------------------------------" + url);
+    fetch(url, {
+      method: "PUT",
+      mode: "cors",
+      body: "",
+      headers: { "Content-Type": "application/json" },
+    }).then((response) => getMessagesFromServer());
   }
 
   useEffect(() => {
@@ -41,13 +66,19 @@ function MessageModel(props) {
   }, [props.name, props.msgArrived]);
 
   return (
-    <div className={styles.container}>
-      <div className="card" style={{ width: "21rem" }}>
+    // <div className={styles.container}>
+    <>
+      <div
+        className="card"
+        style={{ marginTop: "10px", width: "484px", marginLeft: "10px" }}
+      >
         <div className={`card-body ${styles.body}`}>
           <span
             className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
             onClick={props.closeModel}
-            style={{ cursor: "pointer" }}
+            style={{
+              cursor: "pointer",
+            }}
           >
             <GiCrossMark />
           </span>
@@ -79,23 +110,30 @@ function MessageModel(props) {
 
               <br />
               <div className={styles.msgTime}>{chat.hr + " :" + chat.min}</div> */}
-              <MsgButton chat={chat}></MsgButton>
+              <MsgButton
+                chat={chat}
+                deleteMessage={deleteMessage}
+                setReaction={setReaction}
+              ></MsgButton>
             </p>
           ))}
         </div>
         <div class="card-footer text-center">
           <form onSubmit={sendMessage}>
             <input type="text" id="message" />
-            <input
+            <button
               type="submit"
               className="btn btn-success"
               style={{ marginLeft: "10px" }}
               value="send"
-            />
+            >
+              <AiOutlineSend />
+            </button>
           </form>
         </div>
       </div>
-    </div>
+      {/* </div> */}
+    </>
   );
 }
 
